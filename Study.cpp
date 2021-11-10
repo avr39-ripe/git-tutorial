c817927f5bb27fef9b3a93245d0bd063ba4039ff
Олександр Рибчанський <ribchansky_o@it-step.local>
2021-11-10 21:20:50 +0200

    Change: move output to function

--- Study.cpp
+++ Study.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
-int main()
+void greet()
 {
 	std::cout << "Hellooo, dear Client!!! :)\n";
 	std::cout << "Have a nice day clicking on our programm! :)\n";
+
+	return 0;
+}
+
+int main()
+{
+	great();
 	return 0;
 }	
